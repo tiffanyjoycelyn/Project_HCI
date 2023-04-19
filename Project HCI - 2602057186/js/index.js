d885ab8d461339f7383914286307c572ca4f0a52
tiffany <tiffanylumengkewas@gmail.com>
2023-04-19 13:18:25 +0700

    feat: responsive navbar

---
+++ Project HCI - 2602057186/js/index.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active")
+    navigation.classList.toggle("active")
+})
+
+document.querySelectorAll(".navigation > li > a").forEach(e =>
+    e.addEventListener("click",()=>{
+        hamburger.classList.remove("active")
+        navigation.classList.remove("active")
+    }
+))
