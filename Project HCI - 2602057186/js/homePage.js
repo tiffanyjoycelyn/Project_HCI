b4a4e9619912e39f5d2521994071c6761f0d42d0
tiffany <tiffanylumengkewas@gmail.com>
2023-06-02 01:24:50 +0700

    feat:register page validation still not good

--- Project HCI - 2602057186/js/homePage.js
+++ Project HCI - 2602057186/js/homePage.js
@@ -41,54 +41,5 @@ $(document).ready(function () {
   }
 });
 
-const validateForm = () => {
-  const formButton = document.getElementById("register-button");
-
-  formButton.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    const passwordConfirm = document.getElementById(
-      "password-confirmation"
-    ).value;
-    const fullName = document.getElementById("full-name").value;
-    const maleGender = document.getElementById("maleGender").checked;
-    const femaleGender = document.getElementById("femaleGender").checked;
-    const email = document.getElementById("email").value;
-    const age = document.getElementById("age").value;
-    const agree = document.getElementById("agree");
-    const error = document.getElementById("error");
-
-    error.innerText = "";
-
-    console.log(maleGender);
-    console.log(femaleGender);
-
-    if (username === "") {
-      error.innerText = "Username must not be empty";
-    } else if (password === "" || password.length < 5) {
-      error.innerText =
-        "Password must not be empty and length must be greater than 5";
-    } else if (password !== passwordConfirm) {
-      error.innerText = "Password must match the password confirmation";
-    } else if (fullName == "") {
-      error.innerText = "Fullname must not be empty";
-    } else if (maleGender === false && femaleGender === false) {
-      error.innerText = "Gender must be chosen";
-    } else if (email === "" || !email.endsWith(".com")) {
-      error.innerText =
-        "Email not be empty and must ended with '.com' (without quote)";
-    } else if ((age === "") | (Number.parseInt(age) < 16)) {
-      error.innerText = "Age must not be empty and must be greater than 16";
-    } else if (!agree.checked) {
-      error.innerText = "You must agree to our terms and conditions";
-    } else {
-      error.style.color = "green";
-      error.innerText = "Register Success!";
-    }
-  });
-};
-
 runSlider();
-validateForm();
+
