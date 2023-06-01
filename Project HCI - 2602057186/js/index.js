ae30b2fca1a090fbbcb20450bf05ac483c480c6e
tiffany <tiffanylumengkewas@gmail.com>
2023-06-01 20:29:33 +0700

    feat: completed gallery page, with no bug

--- Project HCI - 2602057186/js/index.js
+++ Project HCI - 2602057186/js/index.js
@@ -1,14 +1,14 @@
 const hamburger = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
 
-hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active")
-    navigation.classList.toggle("active")
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
 
-document.querySelectorAll(".navigation > li > a").forEach(e =>
-    e.addEventListener("click",()=>{
-        hamburger.classList.remove("active")
-        navigation.classList.remove("active")
-    }
-))
+document.querySelectorAll(".navigation > li > a").forEach((e) =>
+  e.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navigation.classList.remove("active");
+  })
+);
